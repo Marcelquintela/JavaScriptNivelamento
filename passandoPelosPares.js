2efecb5a812dc60762f556a4adc299e9bd78edf4
Marcel Quintela <marcelquintela@yahoo.com.br>
2020-05-28 20:05:46 -0300

    Create passandoPelosPares.js

---
+++ passandoPelosPares.js
@@ -0,0 +1,9 @@
+// Execute uma função chamada passandoPelosPares() 
+// que mostra pelo console aqui eu tenho o valor de x para cada valor PAR que percorra i de 0 até e inclusive 6.
+
+function passandoPelosPares(){
+    for (var i=0; i<=6; i+=2){
+        console.log("aqui eu tenho o valor de "+i);
+    }
+}
+console.log(passandoPelosPares());
